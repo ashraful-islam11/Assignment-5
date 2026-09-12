7cd21c44237d8038f2daffa9a9ba2724b5dce334
Ashraful Islam <ashraful171790@gmail.com>
2026-09-12 12:56:10 +0600

    Footer section is done

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 
 import Hero from './components/Hero'
 import Nav from './components/Nav'
+import Footer from './Footer'
 
 function App() {
 
@@ -11,6 +12,8 @@ function App() {
      <Nav></Nav>
      <Hero></Hero>
       
+
+      <Footer></Footer>
     </>
   )
 }
